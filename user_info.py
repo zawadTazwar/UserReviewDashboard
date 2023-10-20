5bbe227f99e7e418133251923c8d741e6f434592
gmahm <mgmchowdhury@mun.ca>
2023-10-20 15:45:24 -0230

    Implementation of user_info and session_management for the password and username authentication functions.

--- user_info.py
+++ user_info.py
@@ -1,22 +1,55 @@
-users = {
-    'user1': {
-        'password': 'password1',
-        'email': 'user1@example.com',
-    },
-    'user2': {
-        'password': 'password2',
-        'email': 'user2@example.com',
-    },
-}
+from pymongo import MongoClient
 
-def register_user(username, password, email):
-    if username in users:
-        return False  # User already exists
-    users[username] = {'password': password, 'email': email}
-    return True
+# MongoDB setup
+cluster = MongoClient("mongodb+srv://mahadmirza545:Mahad1234@cluster0.yqjy6mb.mongodb.net/?retryWrites=true&w=majority")
+db = cluster["userreview"]
+users_collection = db["login"]
+
+def register_user(first_name, last_name, username, email, password):
+    """
+    Registers a new user in the MongoDB database.
+
+    Args:
+        first_name (str): The user's first name.
+        last_name (str): The user's last name.
+        username (str): The chosen username.
+        email (str): The user's email address.
+        password (str): The user's password.
+
+    Returns:
+        str: A success message if registration is successful, or an error message if the username already exists.
+    """
+    existing_user = users_collection.find_one({"username": username})
+    if existing_user:
+        return "Username already exists. Please choose a different username."
+
+    user_count = users_collection.count_documents({})
+    user_id = user_count + 1
+
+    user = {
+        "id": user_id,
+        "first_name": first_name,
+        "last_name": last_name,
+        "username": username,
+        "email": email,
+        "password": password  # In a real application, hash the password for security
+    }
+
+    users_collection.insert_one(user)
+    return f"Sign-up successful for {first_name} {last_name} with username {username}"
 
 def authenticate_user(username, password):
-    user = users.get(username)
-    if user and user['password'] == password:
+    """
+    Authenticates a user by checking the provided username and password.
+
+    Args:
+        username (str): The username provided during login.
+        password (str): The password provided during login.
+
+    Returns:
+        bool: True if the username and password are correct, False otherwise.
+    """
+    user = users_collection.find_one({"username": username})
+    if user and user["password"] == password:
         return True
     return False
